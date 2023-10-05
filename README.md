ce2d4405399c3d751393f7ba42c18df8815285f1
avner84 <avner84@gmail.com>
2023-10-05 22:14:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# REST-API---Mini-Social-Network
---
+++ backend/app.js
@@ -0,0 +1,84 @@
+const path = require('path');
+require('dotenv').config();
+const { v4: uuidv4 } = require('uuid');
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + '-' + file.originalname); // הוספת UUID
+  }
+});
+
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  writeConcern: {
+    w: 'majority',
+    j: true
+  }
+}
+)
+  .then(result => {
+    console.log('Connected to MongoDB successfully!');
+    app.listen(8080, () => {
+      console.log('Server is running on port 8080');
+    });
+  })
+  .catch(err => {
+    console.log('Error connecting to MongoDB:', err);
+  });
+
